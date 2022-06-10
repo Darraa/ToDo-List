59e2523e97b54928d45cca364ecf424cf09054e4
Daria Paliy <d.palij111@gmail.com>
2022-06-10 20:36:03 +0300

    add new input
    -add greeting;
    -add category

--- js/main.js
+++ js/main.js
@@ -1,67 +1,115 @@
 window.addEventListener('load', () => {
-   const form = document.querySelector("#new-task-form");
-   const input = document.querySelector("#new-task-input");
-   const list_el = document.querySelector("#tasks");
+   todos = JSON.parse(localStorage.getItem('todos')) || [];
+   const nameInput = document.querySelector("#name");
+   const newTodoForm = document.querySelector("#new-todo-form");
 
-   form.addEventListener('submit', (e) => {
+   const username = localStorage.getItem('username') || '';
+
+   nameInput.value = username;
+
+   nameInput.addEventListener('change', e => {
+      localStorage.setItem('username', e.target.value);
+   })
+
+   newTodoForm.addEventListener('submit', e => {
       e.preventDefault();
 
-      const task = input.value;
-
-      if (!task) {
-         alert("Add the task");
-         return;
+      const todo = {
+         content: e.target.elements.content.value,
+         category: e.target.elements.category.value,
+         done: false,
+         createdAt: new Date().getTime()
       }
 
-      const task_el = document.createElement("div");
-      task_el.classList.add("task");
+      todos.push(todo);
 
-      const task_content_el = document.createElement("div");
-      task_content_el.classList.add("content");
+      localStorage.setItem('todos', JSON.stringify(todos));
 
-      task_el.appendChild(task_content_el);
+      e.target.reset();
 
-      const task_input_el = document.createElement("input");
-      task_input_el.classList.add("text");
-      task_input_el.type = "text";
-      task_input_el.value = task;
-      task_input_el.setAttribute("readonly", "readonly");
+      DisplayTodos();
+   })
+})
 
-      task_content_el.appendChild(task_input_el);
+function DisplayTodos() {
+   const todoList = document.querySelector('#todo-list');
 
-      const task_actions_el = document.createElement("div");
-      task_actions_el.classList.add("actions");
+   todoList.innerHTML = '';
 
-      const task_edit_el = document.createElement("button");
-      task_edit_el.classList.add("edit");
-      task_edit_el.innerHTML = "Edit";
+   todos.forEach(todo => {
+      const todoItem = document.createElement('div')
+      todoItem.classList.add('todo-item');
 
-      const task_delete_el = document.createElement("button");
-      task_delete_el.classList.add("delete");
-      task_delete_el.innerHTML = "Delete";
+      const label = document.createElement('label');
+      const input = document.createElement('input');
+      const span = document.createElement('span');
+      const content = document.createElement('div');
+      const actions = document.createElement('div');
+      const edit = document.createElement('button');
+      const deleteButton = document.createElement('button');
 
-      task_actions_el.appendChild(task_edit_el);
-      task_actions_el.appendChild(task_delete_el);
+      input.type = 'checkbox';
+      input.checked = todo.done; //task
+      span.classList.add('bubble');
 
-      task_el.appendChild(task_actions_el);
+      if (todo.category == 'personal') {
+         span.classList.add('personal');
+      } else {
+         span.classList.add('business');
+      }
 
-      list_el.appendChild(task_el);
+      content.classList.add('todo-content');
+      actions.classList.add('actions');
+      edit.classList.add('edit');
+      deleteButton.classList.add('delete');
 
-      input.value = "";
+      content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+      edit.innerHTML = 'Edit';
+      deleteButton.innerHTML = 'Delete';
 
-      task_edit_el.addEventListener('click', () => {
-         if (task_edit_el.innerText.toLowerCase() == "edit") {
-            task_input_el.removeAttribute("readonly");
-            task_input_el.focus();
-            task_edit_el.innerText = "Save";
+      label.appendChild(input);
+      label.appendChild(span);
+      actions.appendChild(edit);
+      actions.appendChild(deleteButton);
+      todoItem.appendChild(label);
+      todoItem.appendChild(content);
+      todoItem.appendChild(actions);
+
+      todoList.appendChild(todoItem);
+
+      if (todo.done) {
+         todoItem.classList.add('done');
+      }
+
+      input.addEventListener('click', e => {
+         todo.done = e.target.checked;
+         localStorage.setItem('todos', JSON.stringify(todos));
+
+         if (todo.done) {
+            todoItem.classList.add('done');
          } else {
-            task_input_el.setAttribute("readonly", "readonly");
-            task_edit_el.innerText = "Edit";
+            todoItem.classList.remove('done');
          }
+
+         DisplayTodos();
       })
 
-      task_delete_el.addEventListener('click', () => {
-         list_el.removeChild(task_el);
+      edit.addEventListener('click', e => {
+         const input = content.querySelector('input');
+         input.removeAttribute('readonly');
+         input.focus();
+         input.addEventListener('blur', e => {
+            input.setAttribute('readonly', true);
+            todo.content = e.target.value;
+            localStorage.setItem('todos', JSON.stringify(todos));
+            DisplayTodos();
+         })
+      })
+
+      deleteButton.addEventListener('click', e => {
+         todos = todos.filter(t => t != todo);
+         localStorage.setItem('todos', JSON.stringify(todos));
+         DisplayTodos();
       })
    })
-}) 
+}
